4929984682b4acb5294da38d12451ae70e76c8b4
Mariana Grisales Muñoz <marianagrisalesmunoz@gmail.com>
2022-11-16 13:44:58 +0100

    Moved files

---
+++ components/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="container-about">
+            <h2>About</h2>
+            <p>I'm a former languages teacher that decided to go for a career change. As a Frontend Developer, I believe that websites have to be intuitive, clean, and aesthetically appealing, but still fun to interact with.</p>
+        </div>
+        
+    )
+}
---
+++ components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="container-interests">
+            <h2>Interests</h2>
+            <p>Animal expert! Singer and dancer (not a good one, but that doesn't stop me 😂). Languages geek. Art aficionado. Travel fanatic. Italian food lover.</p>
+        </div>
+    )
+}
